ee2aaec48f8c404b752e8b990719117af040c20f
Gamzakov <13555583+Gamzakov@users.noreply.github.com>
2022-03-04 09:47:37 +0300

    Инициализировать проект
    * Добавить .gitignore
    * Добавить решение
    * Добавить шаблоны проектов
    * Добавить шаблоны тестов для каждого проекта

---
+++ src/AutoCompleteTask/AutoComplete/AutoComplete.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace AutoComplete
+{
+
+    public struct FullName
+    {
+        public string Name;
+        public string Surname;
+        public string Patronymic;
+    }
+
+    public class AutoCompleter
+    {
+
+        public void AddToSearch(List<FullName> fullNames)
+        {
+            //Добавьте новый элемент чтобы он начал участвовать в поиске по фио
+        }
+
+        public List<string> Search(string prefix)
+        {
+            //Реализуйте алгоритм поиска по префиксу фио, как составить фио смотри условия задачи.
+        }
+    }
+}
