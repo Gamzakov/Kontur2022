4401036f832d3cb4d6e904883230f8fa8ce0c9e3
Gamzakov <13555583+Gamzakov@users.noreply.github.com>
2022-03-06 19:29:03 +0300

    [Morphology] Реализовать SentenceMorpher
    * Реализовать генератор простых чисел
    * Реализовать добавление словаря
    * Реализовать преобразование слов
    * Добавить шаблон unit-тестов (always failed)

---
+++ src/MorphologyTask/Morphology.Tests/SentenceMorpherTests.cs
@@ -0,0 +1,41 @@
+﻿using Morphology;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace Morphology.Tests
+{
+    [TestFixture]
+    public class SentenceMorpherTests
+    {
+        [Test]
+        public void Create_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            IEnumerable<string> dictionaryLines = null;
+            var sentenceMorpher = SentenceMorpher.Create(dictionaryLines);
+
+            // Act
+            
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Morph_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            IEnumerable<string> dictionaryLines = null;
+            var sentenceMorpher = SentenceMorpher.Create(dictionaryLines);
+
+            // Act
+
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
